73450e3b6e2fe4af482ccd3112d80b3be7a0677a
Klaus Wilhelmi <klaus.wilhelmi@detim.de>
2024-01-21 12:23:40 +0100

    add rocket application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+    format!("Hello, world from {}! I was compiled on {}.", CURRENT_PLATFORM, COMPILED_ON)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
